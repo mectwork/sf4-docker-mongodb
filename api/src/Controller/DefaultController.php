5b9aba1f25ad8e196f0b6bea53dd65b7bf94ccd8
Manuel E Carpio <mectwork@gmail.com>
2017-12-07 21:32:39 -0500

    Declared routes using annotation after "composer req annotation" executed.

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController
 {
     public function index()
     {
         return new Response("<h1>I a fresh Symfony 4 Installation</h1>");
+    }
+
+    /**
+     * @Route("/hello")
+     * @param $name
+     * @return Response
+     */
+    public function hello($name)
+    {
+        return new Response("Hello " . $name);
     }
 }
