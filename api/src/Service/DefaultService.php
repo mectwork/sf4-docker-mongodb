8dd06c844c0af4186bd4e29c5c4730cbf1797741
Manuel Emilio Carpio <mectwork@gmail.com>
2017-12-08 12:29:20 -0500

    Created service to get all routes.

--- api/src/Service/DefaultService.php
+++ api/src/Service/DefaultService.php
@@ -2,12 +2,40 @@
 
 namespace App\Service;
 
+use Symfony\Component\Routing\RouterInterface;
 
-class DefaultService
-{
-    public function theDate()
-    {
-        return date('m/d/Y');
-    }
+class DefaultService {
+
+	/**
+	 * @var RouterInterface
+	 */
+	private $router;
+
+	function __construct( RouterInterface $router ) {
+		$this->router = $router;
+	}
+
+	public function theDate() {
+		return date( 'm/d/Y' );
+	}
+
+    /**
+     * Get all the Routes.
+     *
+     * @return array
+     */
+	public function getRoutes() {
+
+		$collection = $this->router->getRouteCollection();
+		$allRoutes  = $collection->all();
+
+		$paths = [];
+
+		foreach ( $allRoutes as $route ) {
+            $paths[] = $route->getPath();
+		}
+
+		return $paths;
+	}
 
 }
